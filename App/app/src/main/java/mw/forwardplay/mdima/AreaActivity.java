f809e6e45c2a27233b1282fb60b97ba35fb8894b
Andrew Mfune <andiemfune@gmail.com>
2019-03-05 14:20:06 +0800

    Adde location activity

---
+++ App/app/src/main/java/mw/forwardplay/mdima/AreaActivity.java
@@ -0,0 +1,13 @@
+package mw.forwardplay.mdima;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_area);
+    }
+}
