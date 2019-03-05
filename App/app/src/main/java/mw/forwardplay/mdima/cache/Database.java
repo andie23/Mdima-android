8271f881c38fa6982861e235268aa19267bfd83f
Andrew Mfune <andiemfune@gmail.com>
2019-03-05 20:28:23 +0800

    database instance is now retrievable from the database class

--- App/app/src/main/java/mw/forwardplay/mdima/cache/Database.java
+++ App/app/src/main/java/mw/forwardplay/mdima/cache/Database.java
@@ -1,11 +1,24 @@
 package mw.forwardplay.mdima.cache;
 
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 @android.arch.persistence.room.Database(entities = {GroupEntity.class, LocationEntity.class,
         MonitoredLocationEntity.class, RegionEntity.class, ScheduleEntity.class
 }, version = 1, exportSchema = false)
 public abstract class Database extends RoomDatabase {
+    public static final String DATABASE_NAME = "mdima_local_cache";
+    private static Database INSTANCE = null;
+
+    public static Database getInstance(Context context)
+    {
+        if (INSTANCE==null)
+        {
+            INSTANCE = Room.databaseBuilder(context, Database.class, DATABASE_NAME).build();
+        }
+        return INSTANCE;
+    }
     public abstract GroupDao groupDao();
     public abstract  LocationDao locationDao();
     public abstract  MonitoredLocationDao monitoredLocationDao();
