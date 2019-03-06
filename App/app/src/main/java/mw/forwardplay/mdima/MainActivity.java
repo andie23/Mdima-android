f59004b0e4b2faec6f6135ec2898a20078051cc8
Andrew Mfune <andiemfune@gmail.com>
2019-03-06 22:50:31 +0800

    Replaced cardview for main menu with a simple list view

--- App/app/src/main/java/mw/forwardplay/mdima/MainActivity.java
+++ App/app/src/main/java/mw/forwardplay/mdima/MainActivity.java
@@ -5,23 +5,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.List;
 
 import mw.forwardplay.mdima.cache.MdimaDatabase;
 import mw.forwardplay.mdima.cache.RegionEntity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView mainOptions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MdimaDatabase dbInstance = MdimaDatabase.getInstance(this);
-        Log.d("MDIM DATABASE", dbInstance.areaDao().fetchAll().toString());
+        listViewOnClickListerner();
     }
 
-    public void openPrimaryLocation(View view)
+    public void listViewOnClickListerner()
     {
-        Intent areaViewIntent = new Intent(MainActivity.this, AreaViewActivity.class);
-        startActivity(areaViewIntent);
+        mainOptions = (ListView) findViewById(R.id.fixed_main_options);
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+               if(i == 0){
+                   Intent areaViewIntent = new Intent(MainActivity.this, AreaViewActivity.class);
+                   startActivity(areaViewIntent);
+               }
+            }
+        };
+
+        mainOptions.setOnItemClickListener(itemClickListener);
+
     }
+
 }
